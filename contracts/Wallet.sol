a119f1c7572b88e9b22f8045c9ebba4230ae5365
Loïc Ghijselings <l.ghijselings@student.helmo.be>
2023-02-14 12:34:43 +0100

    Smart contract done

--- contracts/Wallet.sol
+++ contracts/Wallet.sol
@@ -5,5 +5,25 @@ pragma solidity ^0.8.9;
 // import "hardhat/console.sol";
 
 contract Wallet {
+
+    mapping(address => uint) public Wallets;
+
+    function withDrawMoney(address payable _to, uint _amount) external {
+        require(_amount <= Wallets[msg.sender], "Not enough funds");
+        Wallets[msg.sender] -= _amount;
+        _to.transfer(_amount);
+    }
+
+    function getBalance() external view returns (uint) {
+        return Wallets[msg.sender];
+    }
     
+    receive() external payable {
+        Wallets[msg.sender] += msg.value;
+    }
+
+    fallback() external payable {
+        
+    }
+
 }
